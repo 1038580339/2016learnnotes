e545f82104912b94aa2d32a4d3ea544f61a1f952
Eccentric <1038580339@qq.com>
2016-11-17 23:04:27 +0800

    react

---
+++ 20161117/main.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Eccentric on 2016/11/17.
+ */
+(function () {
+    
+    ReactDOM.render(
+        <div style={{width:"100%",height:"300px"}}>
+            <div class="listView" style={{height:"100px"}}>
+            <img src="1.png" width="40px" height="40px" style={{float:"left"}}/>
+            <h3 style={{float:"left"}}>订阅号</h3>
+            <p style={{color:"grey"}}>她刊：林丹出轨：愿你能有嫁给嫁给爱情</p>
+                </div>
+            <div class="listView" style={{height:"100px"}}>
+                <img src="1.png" width="40px" height="40px" style={{float:"left"}}/>
+                <h3 style={{float:"left"}}>订阅号</h3>
+                <p style={{color:"grey"}}>她刊：林丹出轨：愿你能有嫁给嫁给爱情</p>
+            </div>
+            <div class="listView" style={{height:"100px"}}>
+                <img src="1.png" width="40px" height="40px" style={{float:"left"}}/>
+                <h3 style={{float:"left"}}>订阅号</h3>
+                <p style={{color:"grey"}}>她刊：林丹出轨：愿你能有嫁给嫁给爱情</p>
+            </div>
+           </div>,
+
+        // <div class="listView">
+        //     <img name="1.png"/>
+        // </div>,
+        //
+        document.querySelector("#root")
+    );
+
+    // function img(props) {
+    //     return <img src={props.name}/>
+    // }
+    // function h(props) {
+    //     return <h3>{props.content}</h3>
+    // }
+    // function p(props) {
+    //     return <p>{props.content}</p>
+    // }
+})();
